e7d02bf2805ff8202225671bb6828a65c17c9810
José Isaac <joseisaacnascimento@gmail.com>
2025-10-02 15:08:42 -0300

    feat: adicionar slogan no Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,12 @@
 export function Hero() {
   return (
-    <section>
-      <h1>lorem</h1>
+    <section className="min-h-screen flex flex-col items-center justify-center bg-black">
+      {/* Imagem da Logo */}
+
+      {/* Textos */}
+      <h1 className="text-3xl md:text-5xl max-w-3xl text-center font-bold text-white">
+        Transformamos dados em decisões inteligentes
+      </h1>
     </section>
   );
 }
