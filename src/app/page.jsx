cf1fad7df1fff068347679be08103c2f7fe31e82
José Isaac <joseisaacnascimento@gmail.com>
2025-10-01 21:40:05 -0300

    setup inicial do projeto com estrutura, libs, fontes e paleta de cores

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+      voluptatibus dolorum ipsa assumenda eveniet? Dolorum harum iste, maxime,
+      perspiciatis libero officiis modi quos facere, nam vitae dolorem. Ipsum,
+      enim suscipit!
+    </h1>
+  );
+}
