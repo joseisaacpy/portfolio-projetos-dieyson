70ed126c2a4b70bf04ba618b180d7bbb09bde6cf
José Isaac <joseisaacnascimento@gmail.com>
2025-10-03 20:34:49 -0300

    feat: criar seção Sobre com textos definidos pelo cliente

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -1,11 +1,11 @@
 export function Sobre() {
   return (
     <section>
-      <h1>
+      <h1 className="text-white">
         Nosso propósito é transformar dados em insights estratégicos, criando
         dashboards e análises que apoiam decisões assertivas.
       </h1>
-      <p>
+      <p className="text-white">
         Combinamos expertise em Business Intelligence para entregar soluções
         visuais, claras e eficazes. Nosso foco é simplificar a complexidade dos
         números e gerar valor real para o negócio.
