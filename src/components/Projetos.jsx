e32f81da972c94f2dc31f1d9c7e5f7068f1f2dfd
José Isaac <joseisaacnascimento@gmail.com>
2025-10-03 20:36:45 -0300

    feat: criar seção de projetos com retorno de cards de projetos placeholders

---
+++ src/components/Projetos.jsx
@@ -0,0 +1,31 @@
+// Importa o objeto de projetos
+import { projetos } from "@/data/projetos";
+
+// Importa o card de projeto
+import { CardProjeto } from "./CardProjeto";
+
+export function Projetos() {
+  return (
+    // Section
+    <section className="bg-gradient-to-b from-verde-padrao to-verde-escuro py-16 px-6">
+      <div className="max-w-6xl mx-auto">
+        {/* Título */}
+        <h2 className="text-4xl font-bold text-center text-white mb-8">
+          Projetos
+        </h2>
+        {/* Grid de projetos  */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {projetos.map((projeto, index) => (
+            <CardProjeto
+              key={index}
+              titulo={projeto.titulo}
+              descricao={projeto.descricao}
+              linkDemo={projeto.linkDemo}
+              imagem={projeto.imagem}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
