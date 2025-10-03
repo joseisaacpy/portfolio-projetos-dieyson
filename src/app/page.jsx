f126beb5ba248384c469971dff55df8431137766
José Isaac <joseisaacnascimento@gmail.com>
2025-10-03 20:40:44 -0300

    feat: criar loading e importar em page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,32 +4,41 @@
 import { Element } from "react-scroll";
 
 // Importa os componetes
-import { Hero } from "@/components/Hero";
 import { Header } from "@/components/Header";
+import { Hero } from "@/components/Hero";
+import { Sobre } from "@/components/Sobre";
+import { Projetos } from "@/components/Projetos";
 import { Footer } from "@/components/Footer";
+import Loading from "@/components/Loading";
 
 // Página Completa com seções/componentes
 export default function Page() {
   return (
     <div className="min-h-screen bg-black">
       {/* Header */}
-      <Element name="header">
+      {/* <Element name="header">
         <Header />
-      </Element>
+      </Element> */}
 
       {/* Hero */}
-      <Element name="home">
+      {/* <Element name="home">
         <Hero />
-      </Element>
+      </Element> */}
 
       {/* Sobre */}
-      <Element name="sobre"></Element>
+      {/* <Element name="sobre">
+        <Sobre />
+      </Element> */}
 
       {/* Projetos */}
-      <Element name="projetos"></Element>
+      {/* <Element name="projetos">
+        <Projetos />
+      </Element> */}
 
       {/* Footer */}
-      <Footer />
+      {/* <Footer /> */}
+
+      <Loading />
     </div>
   );
 }
