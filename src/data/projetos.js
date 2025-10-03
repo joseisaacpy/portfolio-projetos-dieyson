3e46c95de82e59337e76ce412c2716f6def522f1
José Isaac <joseisaacnascimento@gmail.com>
2025-10-03 19:00:21 -0300

    feat: criar mock de dados de projetos

---
+++ src/data/projetos.js
@@ -0,0 +1,50 @@
+export const projetos = [
+  {
+    titulo: "Dashboard de Vendas",
+    descricao:
+      "Painel em Power BI para análise de vendas por região e produto.",
+    imagem:
+      "https://img.freepik.com/vetores-premium/modelo-de-painel-de-usuario-de-painel-de-vetor-vactor-ui-elementos-ux-vactor-analytics_758470-598.jpg",
+    linkDemo: "https://app.powerbi.com/view?r=XXX",
+  },
+  {
+    titulo: "Análise de RH",
+    descricao:
+      "Relatório interativo mostrando turnover, absenteísmo e performance.",
+    imagem:
+      "https://img.freepik.com/vetores-premium/modelo-de-painel-de-usuario-de-painel-de-vetor-vactor-ui-elementos-ux-vactor-analytics_758470-598.jpg",
+    linkDemo: "https://app.powerbi.com/view?r=YYY",
+  },
+  {
+    titulo: "Previsão de Estoque",
+    descricao:
+      "Modelo em Python + Power BI para prever demanda e otimizar estoque.",
+    imagem:
+      "https://img.freepik.com/vetores-premium/modelo-de-painel-de-usuario-de-painel-de-vetor-vactor-ui-elementos-ux-vactor-analytics_758470-598.jpg",
+    linkDemo: "https://app.powerbi.com/view?r=ZZZ",
+  },
+  {
+    titulo: "Previsão de Estoque",
+    descricao:
+      "Modelo em Python + Power BI para prever demanda e otimizar estoque.",
+    imagem:
+      "https://www.zimaltec.es/img/personalizacion/zimaltec/noticias/noticia-powe-bi-potencial-3-min.jpg",
+    linkDemo: "https://app.powerbi.com/view?r=ZZZ",
+  },
+  {
+    titulo: "Previsão de Estoque",
+    descricao:
+      "Modelo em Python + Power BI para prever demanda e otimizar estoque.",
+    imagem:
+      "https://www.zimaltec.es/img/personalizacion/zimaltec/noticias/noticia-powe-bi-potencial-3-min.jpg",
+    linkDemo: "https://app.powerbi.com/view?r=ZZZ",
+  },
+  {
+    titulo: "Previsão de Estoque",
+    descricao:
+      "Modelo em Python + Power BI para prever demanda e otimizar estoque.",
+    imagem:
+      "https://www.zimaltec.es/img/personalizacion/zimaltec/noticias/noticia-powe-bi-potencial-3-min.jpg",
+    linkDemo: "https://app.powerbi.com/view?r=ZZZ",
+  },
+];
