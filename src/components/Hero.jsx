fd6cd38ee3eaf6d9a5a00e9ef9ad9555f66e7469
José Isaac <joseisaacnascimento@gmail.com>
2025-10-03 22:24:14 -0300

    feat: adicionar icon para descer a página

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,23 @@
+import { FaHandPointDown } from "react-icons/fa";
+
+import { Link } from "react-scroll";
+
 export function Hero() {
   return (
-    <section className="min-h-screen flex flex-col items-center justify-center bg-black">
-      {/* Imagem da Logo */}
-
+    <section className="min-h-screen relative flex flex-col items-center justify-center bg-black">
       {/* Textos */}
       <h1 className="text-3xl md:text-5xl max-w-3xl text-center font-bold text-white">
         Transformamos dados em decisões inteligentes
       </h1>
+      {/* Div de botões */}
+      <Link
+        to="sobre"
+        smooth={true}
+        duration={500}
+        className="absolute bottom-10"
+      >
+        <FaHandPointDown className="text-white text-4xl cursor-pointer font-bold transition-all animate-bounce" />
+      </Link>
     </section>
   );
 }
