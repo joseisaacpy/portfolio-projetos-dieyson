20feabb51d5797cabf56c0eea11b5ba866d5e4df
José Isaac <joseisaacnascimento@gmail.com>
2025-10-04 16:07:26 -0300

    style: aumentar fonte dos textos e ajustar alinhamento entre texto e icon

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -26,9 +26,9 @@ export function Contato() {
     >
       {/* Título */}
       <h1 className="text-4xl md:text-5xl font-bold mb-6 text-center">
-        Fale comigo
+        Fale comigo!
       </h1>
-      <p className="text-center mb-12 max-w-md">
+      <p className="text-center text-lg mb-12 max-w-md">
         Escolha o canal que preferir e entre em contato. Respondo o mais rápido
         possível.
       </p>
@@ -40,7 +40,7 @@ export function Contato() {
           href="https://wa.me/558699525475"
           target="_blank"
           rel="noopener noreferrer"
-          className="flex items-center gap-3 text-lg font-medium bg-cinza-medio/30 px-4 py-2 rounded-lg hover:text-green-500 transition-all"
+          className="flex items-center gap-3 text-xl justify-center font-medium bg-cinza-medio/30 px-4 py-2 rounded-lg hover:text-green-500 transition-all"
         >
           <FaWhatsapp className="text-3xl text-green-500" />
           <span>WhatsApp</span>
@@ -51,7 +51,7 @@ export function Contato() {
           href="https://www.instagram.com/insight.dataanalisys"
           target="_blank"
           rel="noopener noreferrer"
-          className="flex items-center gap-3 text-lg font-medium bg-cinza-medio/30 px-4 py-2 rounded-lg hover:text-pink-500 transition-all"
+          className="flex items-center gap-3 text-xl justify-center font-medium bg-cinza-medio/30 px-4 py-2 rounded-lg hover:text-pink-500 transition-all"
         >
           <FaInstagram className="text-3xl text-pink-500" />
           <span>Instagram</span>
@@ -59,7 +59,7 @@ export function Contato() {
 
         {/* Email */}
         <a
-          className="flex items-center gap-3 cursor-pointer text-lg font-medium bg-cinza-medio/30 px-4 py-2 rounded-lg hover:text-blue-400 transition-all"
+          className="flex items-center gap-3 cursor-pointer text-xl justify-center font-medium bg-cinza-medio/30 px-4 py-2 rounded-lg hover:text-blue-400 transition-all"
           onClick={() => conpiarEmail("")}
         >
           <FaEnvelope className="text-3xl text-blue-400" />
