ac2fbfacc2215bb2ceef5f1349fcf446518d17ab
José Isaac <joseisaacnascimento@gmail.com>
2025-10-04 12:46:50 -0300

    feat: adicionar ano atual no footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,15 @@
+// Importa Link
 import Link from "next/link";
 
 export function Footer() {
   return (
     <footer className="bg-verde-escuro py-6 flex flex-col items-center text-center text-gray-200">
+      {/* Div de textos */}
       <div className="max-w-screen-lg mx-auto flex flex-col gap-1">
+        {/* Textos do footer */}
         <p className="text-sm">
-          Insight Data Analysis | Todos os direitos reservados
+          © {new Date().getFullYear()} Insight Data Analysis | Todos os direitos
+          reservados
         </p>
         <p className="text-sm">
           Desenvolvido por{" "}
