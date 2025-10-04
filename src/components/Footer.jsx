e0dc50e08f0574c29901bdbb80e61e9732792b50
José Isaac <joseisaacnascimento@gmail.com>
2025-10-03 22:06:12 -0300

    style: adicionar mais espaçamento no footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,23 @@
-// Importa Link
 import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="bg-verde-escuro py-2 flex flex-col text-center text-white">
-      <p className="text-sm">
-        Insight Data Analisys | Todos os direitos reservados
-      </p>
-      <p className="text-sm">
-        Desenvolvido por{" "}
-        <Link href="https://github.com/joseisaacpy" className="underline">
-          José Isaac
-        </Link>
-      </p>
+    <footer className="bg-verde-escuro py-6 flex flex-col items-center text-center text-gray-200">
+      <div className="max-w-screen-lg mx-auto flex flex-col gap-1">
+        <p className="text-sm">
+          Insight Data Analysis | Todos os direitos reservados
+        </p>
+        <p className="text-sm">
+          Desenvolvido por{" "}
+          <Link
+            href="https://github.com/joseisaacpy"
+            className="underline hover:text-white transition-colors"
+            target="_blank"
+          >
+            José Isaac
+          </Link>
+        </p>
+      </div>
     </footer>
   );
 }
